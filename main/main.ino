6a714f82f9f27709f2d808bd44ed2eae27ab1d87
Ahmed Elbehairy <ahmedelbehairy.72005@gmail.com>
2024-03-27 03:51:24 +0200

    optimized the code

--- main/main.ino
+++ main/main.ino
@@ -1,17 +1,16 @@
-uint8_t back = 40;         //the value when the fire is near
-
 // defining used functions
 bool handleFire();
-void setWheels(uint8_t n);
 
 void setup() {
   //for every pin of the output pins
-  for (uint8_t i = 9; i < 14; i++) {
-    pinMode(i, OUTPUT);            //set the pinmode to output
-  } 
-
-  pinMode(A0, INPUT);       //sensor analog pin
-  pinMode(8, INPUT);        //sensor digital pin
+  // left forward, left reverse, right forward, right reverse, fan, sensor DO
+  // ports:  PB5 PB4 PB3 PB2 PB1 PB0
+  // pins:   13  12  11  10  09  08
+  // binary: 1   1   1   1   1   0   = 62 in decimal
+  
+  DDRB = (1 << PB5) | (1 << PB4) | (1 << PB3) | (1 << PB2) | (1 << PB1);
+  PORTB = (1 << PB0);       // set a pullover on pin 0 to read it by pinb
+  ADMUX |= (3 << 6);        // setting the reference for analog read as the internal reference
 
   Serial.begin(9600);         //start serial monitor
  
@@ -19,87 +18,50 @@ void setup() {
  
 void loop() {
 
-  delay(200);             //delay 200 ms
-
+  delay(500);
   //if fire handled
   if (handleFire()) {
     // ignore whatever user sent by bluetooth and continue the loop
     return;
   }
 
-  //if nothing sent by bluetooth
+  //if not recieved command from bluetooth
   if (!Serial.available()) {
-    setWheels(0);         //stop the car
-    return;             //end the loop
+    PORTB = 0;
+    return;
   }
-    
-  setWheels(Serial.read());                   //modify the wheels movement as got from the serial
+  // forward 4 => 40
+  // backward * => 20
+  // right 0 => 32
+  // left " => 8
+  PORTB = (Serial.read() & 31) << 1;                   //modify the wheels movement as got from the serial
+  Serial.read();                                // get rid of the line feed got from the serial	
 }
 
 bool handleFire() {
-
-  // return if no fire detected
-  if (!digitalRead(8)) {
-    digitalWrite(9, 0);
+	
+  // return if no fire detected from sensor digital input
+  if (!PINB0) {
+    // make sure fan is off
+    PORTB &= ~(1 << 1);
+    // return false to tell the arduino to complete it's job
     return false;
   }
 
-  //read the sensor
-  int read = analogRead(A0);
-  Serial.print("sensor: ");
+  //read the sensor analog data
+  ADCSRA |= (3 << 6);       //starting an analog read
+  while(bit_is_set(ADCSRA, ADSC));      // waiting for the read to complete
+  short read = ADCL | (ADCH << 8);      // setting the variable read to the value read from A0
   Serial.println(read);
 
-  //if fire is so high go back   
-  if (read <= back) {
-    setWheels(42);
-  }
-  // if fire in range, stop the car, turn on the fans and put it off
-  else if (read <= back + 4) {
-    setWheels(0);
-    digitalWrite(9, 1);
+  // if fire in range, turn on the fans, stop the car and put it off
+  if (read <= 30) {
+    PORTB = 2;
   }
   // else, go towards the fire
   else {
-    setWheels(37);
+    PORTB = 40;
   }
   // return true to indicate that a fire is detected
   return true;
-}
-
-void setWheels(uint8_t n) {
-
-  /*
-    Binary digits for movement:
-      forward 0101
-      backward 1010
-      right 0001
-      left 0100
-      right reverse 1000
-      left reverse 0010
-      stop 0000
-
-      13 left forward; 
-      12 left reverse;
-      11 right forward;
-      10 right reverse;
-  */
-
-  //for every wheel pin
-  digitalWrite(13, n & 1);
-  digitalWrite(12, n & 2);
-  digitalWrite(11, n & 4);
-  digitalWrite(10, n & 8);
-
-
-  //same functionality with for loop
-
-  // // {left forward, left reverse, right forward, right reverse}
-  // uint8_t wheels[nWheels] = {13, 12, 11, 10};
-
-  // //the binary digits to compare with serial value
-  // uint8_t binaries[nWheels] = {1, 2, 4, 8};
-
-  // for (uint8_t i = 0; i < nWheels; i++) {
-  //   digitalWrite(wheels[i], n & binaries[i]);             //write the corresponding current to it
-  // } 
 }
